ba32f227fad3bb17588bb0c5416465ef71310683
mayankmaria <mayank_maria@hotmail.com>
2019-04-26 14:25:30 +0530

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PGUSER,
+    pgPort: process.env.PGPORT,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgHost: process.env.PGHOST
+};
